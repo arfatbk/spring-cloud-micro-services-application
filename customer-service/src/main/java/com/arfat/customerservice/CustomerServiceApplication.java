a7f8804596a2183644b1bc8812a69812b70e8a21
arfatbk <arfatbk@gmail.com>
2020-09-04 14:36:20 +0530

    Added GET '/' endpoint

--- customer-service/src/main/java/com/arfat/customerservice/CustomerServiceApplication.java
+++ customer-service/src/main/java/com/arfat/customerservice/CustomerServiceApplication.java
@@ -3,13 +3,20 @@ package com.arfat.customerservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @EnableEurekaClient
+@RestController
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerServiceApplication.class, args);
 	}
 
+	@GetMapping
+	public String hello(){
+		return "Hello from customer-service";
+	}
 }
