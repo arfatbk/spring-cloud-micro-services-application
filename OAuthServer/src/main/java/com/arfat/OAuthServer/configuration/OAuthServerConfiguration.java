095eeda63d1b1f023a979ec284a0c8cd1193e190
arfatbk <arfatbk@gmail.com>
2020-09-05 15:24:52 +0530

    OAuth server with hardcoded user and client in properties file

---
+++ OAuthServer/src/main/java/com/arfat/OAuthServer/configuration/OAuthServerConfiguration.java
@@ -0,0 +1,35 @@
+package com.arfat.OAuthServer.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+
+/**
+ * @author Arfat Bin Kileb
+ * Created at 05-09-2020 02:36 PM
+ */
+//@Configuration
+//@EnableAuthorizationServer
+public class OAuthServerConfiguration {//implements AuthorizationServerConfigurer {
+
+
+//    @Override
+//    public void configure(final AuthorizationServerSecurityConfigurer auth) throws Exception {
+//
+//    }
+//
+//
+//    @Override
+//    public void configure(final ClientDetailsServiceConfigurer client) throws Exception {
+//
+//    }
+//
+//    @Override
+//    public void configure(final AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+//
+//    }
+}
