5ea31397b30f552686180f2535740fb4e381e589
arfatbk <arfatbk@gmail.com>
2020-09-09 23:28:14 +0530

    Added AccessTokenJwtRelayFilter

---
+++ API-Gateway/src/main/java/com/arfat/APIGateway/AccessTokenJwtRelayFilter.java
@@ -0,0 +1,80 @@
+package com.arfat.APIGateway;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.Map;
+
+@Log4j2
+public class AccessTokenJwtRelayFilter extends AbstractGatewayFilterFactory<Object> {
+    @Autowired
+    WebClient webClient;
+
+    public AccessTokenJwtRelayFilter() {
+    }
+
+    public GatewayFilter apply() {
+        return this.apply((Object) null);
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+
+            String accessToken = exchange.getRequest().getHeaders().getFirst("auth");
+            log.info("Token Relay===========================" + accessToken);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setBasicAuth("bW9iaWxlOnBpbg==");
+            HttpEntity<String> entity = new HttpEntity<String>(headers);
+
+            //TODO:check if token is null
+            String uri = "http://localhost:8282/oauth/check_token?token=" + accessToken;
+
+            return webClient.get().uri(uri)
+                    .headers(httpHeaders -> httpHeaders.setBasicAuth("bW9iaWxlOnBpbg=="))
+                    .exchange()
+                    .flatMap(clientResponse -> {
+                        if (clientResponse.statusCode().is2xxSuccessful()) {
+
+                            return clientResponse
+                                    .toEntity(Map.class)
+                                    .flatMap(s -> {
+                                        String jwtToken = (String) s.getBody().getOrDefault("user_name", "null");
+                                        System.out.println("user_name =" + jwtToken);
+                                        ServerWebExchange ex = this.withBearerAuth(exchange, jwtToken);
+                                        return chain.filter(ex);
+                                    });
+                        } else {
+                            ServerHttpResponse response = exchange.getResponse();
+                            response.setStatusCode(clientResponse.statusCode());
+                            response.getHeaders().add("X-intercepted", "true");
+                            response.getHeaders().setContentType(MediaType.APPLICATION_JSON);
+
+                            return exchange.getResponse().writeWith(
+                                    clientResponse
+                                            .toEntity(String.class)
+                                            .flatMap(s -> Mono.just(response.bufferFactory().wrap(s.getBody().getBytes())))
+                            );
+                        }
+                    });
+        };
+    }
+
+    private ServerWebExchange withBearerAuth(ServerWebExchange exchange, String token) {
+        return exchange.mutate().request((r) -> {
+            r.headers((headers) -> {
+                headers.setBearerAuth(token);
+            });
+        }).build();
+    }
+}
--- API-Gateway/src/main/java/com/arfat/APIGateway/Router.java
+++ API-Gateway/src/main/java/com/arfat/APIGateway/Router.java
@@ -1,12 +1,14 @@
 package com.arfat.APIGateway;
 
-import org.springframework.cloud.gateway.route.RouteDefinition;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 
 /**
  * @author Arfat Bin Kileb
@@ -16,18 +18,38 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Router {
 
+    @Autowired
+    AccessTokenJwtRelayFilter tokenRelayFilter;
+
     @Bean
     RouteLocator routes(RouteLocatorBuilder builder) {
 
         return builder.routes()
                 .route(r -> r.path("/get")
-                        .filters(f -> f.addResponseHeader("X-Spring-Gateway", "API Gateway"))
-                        .uri("forward:/fallback "))
+                        .filters(f -> f
+                                .addResponseHeader("X-Spring-Gateway", "API Gateway")
+                                .setPath("/fallback")
+                                .filter(tokenRelayFilter.apply()))
+                        .uri("http://localhost:8080"))
+                .route(r -> r.path("/fallback")
+                        .uri("localhost:8080"))
                 .build();
     }
 
+    @Bean
+    AccessTokenJwtRelayFilter getRequestBean() {
+        return new AccessTokenJwtRelayFilter();
+    }
+
+    @Bean
+    WebClient webClient() {
+        //Add default header for OAuth server
+        return WebClient.builder().build();
+    }
+
     @RequestMapping("/fallback")
-    public String fallback(){
+    public String fallback(@RequestHeader("Authorization") String str) {
+        System.out.println("str = " + str);
         return "fallback";
     }
 }
